caadd8cd729aef4687342fb4d71587e134806b67
didwogus59 <didwogus59@naver.com>
2023-07-31 10:52:51 +0900

    초기 커밋.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int arr[31] = {};
+void make_line(int n) {
+
+}
+int main(void)
+{
+	int n;
+	cin >> n;
+	while (n != 0) {
+		make_line(n);
+		cin >> n;
+	}
+}
